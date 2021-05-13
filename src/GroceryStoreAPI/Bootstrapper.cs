75fd5e98da2556910dc08ecb267a1c4ce3649590
Rob Mayo <robertmayopro@gmail.com>
2021-05-13 17:27:16 -0400

    Some initial project structure.

---
+++ src/GroceryStoreAPI/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GroceryStoreAPI
+{
+    internal static class Bootstrapper
+    {
+        public static void AddApplicationDependencies(this IServiceCollection services)
+        {
+            // TODO: Add Dependencies here
+        }
+    }
+}
--- src/GroceryStoreAPI/Startup.cs
+++ src/GroceryStoreAPI/Startup.cs
@@ -19,6 +19,8 @@ namespace GroceryStoreAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddApplicationDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
